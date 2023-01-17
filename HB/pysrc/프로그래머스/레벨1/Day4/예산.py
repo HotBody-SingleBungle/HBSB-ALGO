0f32c7bf6c2f2fb6d54a2f7fadecae166be90531
박주윤 _ Jy Park <jy.park@kohyoung.net>
2023-01-17 13:53:02 +0900

    DAY4 - 예산.py

--- HB/pysrc/프로그래머스/레벨1/Day4/예산.py
+++ HB/pysrc/프로그래머스/레벨1/Day4/예산.py
@@ -2,19 +2,11 @@ def solution(d, budget):
     answer = 0
     d.sort()
     sum_=0
-    cnt=0
-    
     for i in range(len(d)):
         sum_+=d[i]
-        cnt+=1
-
-        if sum_ == budget:
-            answer=cnt
-            return answer
-        
-        if sum_>budget:
-            cnt-=1
+        if sum_ <= budget:
+            answer+=1
+        if answer > budget:
+            answer -= 1
             break
-    
-            
     return answer
