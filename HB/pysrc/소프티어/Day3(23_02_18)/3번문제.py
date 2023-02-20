cb0ddde14511ac422509528e934fbcadddad39ba
juyoon <pjyc17@gmail.com>
2023-02-20 23:47:15 +0900

    re

--- HB/pysrc/소프티어/Day3(23_02_18)/3번문제.py
+++ HB/pysrc/소프티어/Day3(23_02_18)/3번문제.py
@@ -10,11 +10,15 @@ for i in perm:
 answer = []
 
 for i in range(len(lst2)//2):
-    a = 0
+    temp = 0
     for j in range(N//2):
+        a = 0
         for k in range(K):
             a += lst[lst2[i][k]][j] - lst[lst2[len(lst2)-1-i][k]][j]
         answer.append(abs(a))
+    temp += answer.pop()
+    temp += answer.pop()
+    temp += answer.pop()
+    answer.append(temp)
 
-
-print(answer)
+print(min(answer))
