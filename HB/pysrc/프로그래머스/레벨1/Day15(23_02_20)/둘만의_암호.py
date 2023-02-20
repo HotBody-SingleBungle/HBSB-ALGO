91816b4edffcf53d373bde4288b6addc927200ab
박주윤 _ Jy Park <jy.park@kohyoung.net>
2023-02-21 08:52:41 +0900

    DAY15(23_02_20)

---
+++ HB/pysrc/프로그래머스/레벨1/Day15(23_02_20)/둘만의_암호.py
@@ -0,0 +1,42 @@
+def solution(s, skip, index):
+    answer = ''
+    skip_idx = [ord(skip[i]) for i in range(len(skip))]
+
+    for j in range(len(s)):
+        cnt = 0
+        cnt_ = 0
+
+        for i in range(index + len(skip)):
+
+            if ord(s[j])+i+1 < 123:
+                if ord(s[j])+i+1 in skip_idx:
+                    cnt_ += 1
+                    pass
+
+                else:
+                    cnt += 1
+
+                    if cnt == index:
+                        if ord(s[j])+i+1 <= 122:
+                            answer += chr(ord(s[j])+cnt+cnt_)
+                        else:
+                            answer += chr(ord(s[j])+cnt+cnt_-26)
+                        break
+
+            else:
+                if ord(s[j])+i-25 in skip_idx:
+                    cnt_ += 1
+                    pass
+                else:
+                    cnt += 1
+
+                    if cnt == index:
+                        if ord(s[j])+cnt-26+cnt_ < 123:
+                            answer += chr(ord(s[j])+cnt-26+cnt_)
+                        else:
+                            answer += chr(ord(s[j])+cnt-52+cnt_)
+                        break
+    print(answer)
+    return answer
+# solution('aukksz', 'xcvtyuighjkbnmiopjklmasdfw', 5)
+# print(ord("}"))
