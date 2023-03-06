e2038346da8275d120db119dfc00ee0fea8e63df
박주윤 _ Jy Park <jy.park@kohyoung.net>
2023-03-06 09:47:13 +0900

    not upload before 230306

--- HB/pysrc/프로그래머스/레벨1/Day18(23_02_27)/대충_만든_자판.py
+++ HB/pysrc/프로그래머스/레벨1/Day18(23_02_27)/대충_만든_자판.py
@@ -1,3 +1,10 @@
 def solution(keymap, targets):
     answer = []
+    target = []
+    for i in targets:
+        target += i
+    target = set(target)
+    print(target)
     return answer
+solution([['a','s','v'],['c','d','e']], [['a','c','e','f'], ['g','y','e']])
+print('asdf'.index('a'))
